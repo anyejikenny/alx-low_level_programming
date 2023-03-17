0622a9ce083b8b43c8d9b4532da034b3348265e1
Anyeji Michael <anyeji.michael@gmail.com>
2023-03-17 02:07:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 /**
-*main - main entry point
-*return:always 0 (success)
-*/
+ * main - Entry point of the program
+ *
+ * Description: This function performs some task and returns an integer value.
+ *              It serves as the entry point of the program.
+ *
+ * Return: 0 if the program ran successfully, otherwise an error code
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
